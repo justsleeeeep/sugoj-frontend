25d0174bae871618c551dc935bf2b1d8707d8366
qianchen <chen.qian@dningbo.com>
2026-05-06 11:58:29 +0800

    第一个分歧路由改变用afterEach

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="menu-demo">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/oj-logo.png" />
+          <div class="title">SUG-OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRouter, useRoute } from "vue-router";
+import { ref } from "vue";
+const router = useRouter();
+const route = useRoute();
+const selectedKeys = ref(["/"]);
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header"><GlobalHeader /></a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://localhost:8080"> 神秘网址 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+#basicLayout .content {
+  background: linear-gradient(to right, #aaa, #fff);
+  margin-bottom: 16px;
+}
+#basicLayout .footer {
+  background: #efefef;
+  margin-bottom: 16px;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  text-align: center;
+}
+</style>
