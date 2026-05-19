ee19b600ca96f361d9b49dff3b69fc7fbdea3857
qianchen <chen.qian@dningbo.com>
2026-05-19 17:09:59 +0800

    company update

---
+++ src/components/CodeEdit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="code-editor" ref="codeEditorRef" />
+  <Editor :value="modelValue" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script>
+import * as monaco from "monaco-editor";
+const codeEditorRef = ref();
+const myEditor = monaco.editor.create(document.getElementById("container"), {
+  value,
+  language: "javascript",
+  automaticLayout: true,
+});
+</script>
+
+<style></style>
