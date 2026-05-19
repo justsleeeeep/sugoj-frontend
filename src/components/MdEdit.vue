8d9d508367df18caaf26a864d37d27f31f56b267
qianchen <chen.qian@dningbo.com>
2026-05-19 16:39:12 +0800

    company update

---
+++ src/components/MdEdit.vue
@@ -0,0 +1,42 @@
+<template>
+  <Editor :value="modelValue" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script>
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { Editor } from "@bytemd/vue-next";
+
+const plugins = [gfm(), highlight()];
+
+export default {
+  components: { Editor },
+
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+
+  emits: ["update:modelValue"],
+
+  data() {
+    return {
+      plugins,
+    };
+  },
+
+  methods: {
+    handleChange(v) {
+      this.$emit("update:modelValue", v);
+    },
+  },
+};
+</script>
+
+<style>
+.bytemd-toolbar-right > :last-child {
+  display: none;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import MdEdit from "@/components/MdEdit.vue";
+
+const content = ref("");
+
+// 监听 markdown 内容变化
+watch(content, (newValue) => {
+  console.log("md内容:", newValue);
+});
+</script>
+
 <template>
   <div class="about">
+    <MdEdit v-model="content" />
     <h1>This is an about page</h1>
   </div>
 </template>
